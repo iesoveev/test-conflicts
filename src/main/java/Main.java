145dfe3ae2540b0264ab6ae31dc6b11b94932f3f
iesoveev <esoveevi@taptaxi.su>
2020-10-10 23:59:51 +0400

    12345671234567890

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("a");
         System.out.println("b");
+        System.out.println("123456789k");
     }
 }
